3579dfe9de0174165b749e7f414c5de581d81cc1
ArturMiguel <arturmrocha1@gmail.com>
2020-07-07 17:41:21 -0300

    1.0.2

---
+++ README.md
@@ -0,0 +1,7 @@
+# youtube-speed-addon
+
+Firefox add-on to modify YouTube playback rate. 
+
+This extension only injects a new speed into the video, nothing else is created or changed.
+
+<img src='./assets/preview.jpg' alt='img-preview'>
--- js/change.js
+++ js/change.js
@@ -5,7 +5,7 @@ function changeSpeed(request, sender, sendResponse) {
     let speed = document.getElementsByClassName('video-stream html5-main-video')[0].playbackRate;
 
     if (option === 'increment') speed+= 0.25;
-    else if (option === 'decrement') speed-= 0.25;
+    else if (option === 'decrement' && speed > 0) speed-= 0.25;
 
     document.getElementsByClassName('video-stream html5-main-video')[0].playbackRate = speed;
 
